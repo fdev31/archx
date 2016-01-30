70a1df3840355e7e91a00da6d689cba858fa407a
fab <fdev31@gmail.com>
2016-01-30 13:08:39 +0100

    Fix networkmanager installation

--- hooks/all/base/install/50_network_networkmanager.sh
+++ hooks/all/base/install/50_network_networkmanager.sh
@@ -1,3 +1,7 @@
 . ./strapfuncs.sh
 
 install_pkg -Sy --noconfirm networkmanager
+
+ln -sf /usr/lib/systemd/system/NetworkManager.service "$R/etc/systemd/system/dbus-org.freedesktop.NetworkManager.service"
+ln -sf /usr/lib/systemd/system/NetworkManager.service "$R/etc/systemd/system/multi-user.target.wants/NetworkManager.service"
+ln -sf /usr/lib/systemd/system/NetworkManager-dispatcher.service "$R/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service"
