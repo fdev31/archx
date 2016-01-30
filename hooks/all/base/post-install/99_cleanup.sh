ca9d1186aa3b669742fddb7016a1f9f04f49b8c0
fab <fdev31@gmail.com>
2016-01-30 13:49:05 +0100

    don't set hwdb in RAM at boot

--- hooks/all/base/post-install/99_cleanup.sh
+++ hooks/all/base/post-install/99_cleanup.sh
@@ -1,4 +1,8 @@
 source ./configuration.sh
 
+if [ -e "$R/etc/udev/hwdb.bin"]; then
+    mv "$R/etc/udev/hwdb.bin" "$R/usr/lib/udev/hwdb.bin"
+fi
+
 pacman-optimize "$R/var/lib/pacman"
 ldconfig -r "$R"
