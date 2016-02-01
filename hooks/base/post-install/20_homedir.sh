88113639715504f7a84b56905bf53bd131eb453d
fab <fdev31@gmail.com>
2016-02-01 18:16:39 +0100

    include minimal HOME & sync it - with cool theme & background instead of defaults (for cinnamon)

--- hooks/base/post-install/20_homedir.sh
+++ hooks/base/post-install/20_homedir.sh
@@ -1,5 +1,7 @@
 . ./strapfuncs.sh
 
-sudo tar xf ./resources/HOME.tar -C "$R/home/$USERNAME/"
+rm -fr "$R/home/$USERNAME/"
+cp -r ./ressources/HOME "$R/home/$USERNAME"
+
 sudo chown -R 1000.100 "$R/home/$USERNAME"
 
