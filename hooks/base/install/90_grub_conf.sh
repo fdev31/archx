09626b5763035b57acd0b6534cfd64cf555eb03d
fab <fdev31@gmail.com>
2016-02-05 16:22:40 +0100

    rollback problematic grub configuration

--- hooks/base/install/90_grub_conf.sh
+++ hooks/base/install/90_grub_conf.sh
@@ -1,5 +1,7 @@
 source ./strapfuncs.sh
 
+install_pkg grub
+
 sudo mkdir "$R/boot/grub" 2> /dev/null
 sudo install -DT -o root -g root -m 640 /usr/share/grub/euro.pf2 "$R/boot/grub/font.pf2"
 sudo install -g root -o root -m 644 resources/grub.cfg "$R/boot/grub/grub.cfg"
