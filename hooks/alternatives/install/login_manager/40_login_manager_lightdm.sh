8042bca14124421f8131f1349ad19a53a9f450e2
fab <fdev31@gmail.com>
2016-02-09 03:26:23 +0100

    fix lightdm install order to avoid duplicate work

--- hooks/alternatives/install/login_manager/40_login_manager_lightdm.sh
+++ hooks/alternatives/install/login_manager/40_login_manager_lightdm.sh
@@ -1,12 +1,11 @@
-
-install_pkg  lightdm
-
-sudo mkdir "$R/run/lightdm" 2> /dev/null # fix warning
-
 if [ "$PREFERRED_TOOLKIT" = "gtk" ]; then
     install_pkg  lightdm-gtk-greeter
 else
     install_pkg  lightdm-kde-greeter
 fi
+
+sudo mkdir "$R/run/lightdm" 2> /dev/null # fix warning
+
+install_pkg  lightdm
 
 enable_service lightdm
