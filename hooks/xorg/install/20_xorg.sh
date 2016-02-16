c0982c8981e6b8e0936d9a4c7d0914a1988cc399
fab <fdev31@gmail.com>
2016-02-16 20:43:54 +0100

    move code on xorg (executed post-install)

--- hooks/xorg/install/20_xorg.sh
+++ hooks/xorg/install/20_xorg.sh
@@ -7,14 +7,6 @@ copy /etc/X11/xorg.conf.d/10-keyboard-layout.conf
 enable_service accounts-daemon
 if [ "$PREFERRED_TOOLKIT" = "gtk" ]; then
     install_pkg gtk-engines
-    echo "
-[Qt]
-Style=GTK+" | dd of="$R/etc/xdg/Trolltech.conf"
-    chmod 644 "$R/etc/xdg/Trolltech.conf"
-
-    if ! contains QT_STYLE "$R/etc/environment" ; then
-        append "$R/etc/environment" 'QT_STYLE_OVERRIDE=GTK+'
-    fi
 fi
 
 install_pkg libx264 mesa-libgl
