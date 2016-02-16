c94da0d5544365935d72b0f097b417ccc2fc0f9f
fab <fdev31@gmail.com>
2016-02-16 20:46:34 +0100

    Simple fixes / remove error checks temporarily

--- resources/persist_mounter.sh
+++ resources/persist_mounter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RPFX="/mnt/persist"
+RPFX="/mnt/storage"
 
 mount_persist() {
     if [ -e "/run/overlay/.reset_state" ]; then
@@ -18,11 +18,11 @@ myumount() {
 unmount_persist() {
 
     umount /home || exit -1
-    for DIR in /etc /var/db /usr /srv /opt /var/lib; do
-        myumount "$DIR" || exit -1
+    for DIR in /etc /var/db /usr /srv /opt /var/lib/pacman /var/lib; do
+        myumount "$DIR" 
     done
 
-    umount "$RFPX" || exit -1
+    umount "$RFPX"
     sync
     mount -o remount,ro /boot || exit -1
     echo "DATA is SAFE"
