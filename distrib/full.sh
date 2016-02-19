6c81d125b18b6b86710577bd3e41f470b94d2088
fab <fdev31@gmail.com>
2016-02-19 18:31:50 +0100

    add chat & installer to full distrib

--- distrib/full.sh
+++ distrib/full.sh
@@ -1,4 +1,4 @@
-PROFILES='base xorg env-cinnamon env-awesome env-gnome net-utils system snd-base snd-more gfx-base gfx-more photo-base office-base office-more locales medical proprietary dev-base'
+PROFILES='base xorg env-cinnamon env-awesome env-gnome net-utils system snd-base snd-more chat gfx-base gfx-more photo-base office-base office-more locales medical proprietary dev-base installer'
 PREFERRED_TOOLKIT=gtk
 DISTRO_PACKAGE_LIST='firefox-extension-ublock-origin firefox-extension-video-downloadhelper'
 function distro_install_hook() {
