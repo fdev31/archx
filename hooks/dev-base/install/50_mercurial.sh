1e41f7ec5d1322d296e31f1f3ee82bc59445932b
fab <fdev31@gmail.com>
2016-02-19 19:12:19 +0100

    add more stuff to mercurial (tortoise)

--- hooks/dev-base/install/50_mercurial.sh
+++ hooks/dev-base/install/50_mercurial.sh
@@ -1,2 +1,9 @@
 install_pkg mercurial
+
+for d in python2 mercurial python2-pyqt4 python2-qscintilla python2-iniparse; do
+    install_pkg --asdeps "$d"
+done
+
+install_pkg python2-pygments
+
 install_pkg tortoisehg
