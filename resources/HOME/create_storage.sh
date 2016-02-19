c5d9853ec9bd131bd838aac8bf86a82cb1de7c9c
fab <fdev31@gmail.com>
2016-02-19 19:11:27 +0100

    update storage creation script

--- resources/HOME/create_storage.sh
+++ resources/HOME/create_storage.sh
@@ -5,8 +5,6 @@ BOOTPART=$(mount 2>/dev/null | grep " $R/boot ")
 BOOTPART=${BOOTPART%% *}
 BOOTDRIVE=${BOOTPART%?}
 
-sudo partx "$BOOTDRIVE"
-
 DISKLABEL=$(blkid $BOOTPART -s LABEL -p)
 DISKLABEL=${DISKLABEL#*\"}
 DISKLABEL=${DISKLABEL%\"*}
@@ -14,16 +12,9 @@ DISKLABEL=${DISKLABEL%\"*}
 zenity --question --text "Proceed installing on $DISKLABEL (${BOOTDRIVE}2) ?"
 
 if [ "$?" -ne "0" ]; then
-	exit 1
+    echo "Canceled"
+	exit 255
 fi
-
-echo $BOOTDRIVE
-
-oops() {
-	echo "Error ! $*"
-	exit -1
-}
-
 
 case $FSTYPE in
 btr*)
@@ -46,26 +37,37 @@ btr*)
 esac
 
 PARTCOUNT=$(ls $BOOTDRIVE* | wc -l)
-if [ "$PARTCOUNT" -eq "3" ]; then # default 2 partition mode
-	# extend partition
-	echo "d\n2\nn\np\n\n\n\n\nw" | sudo LC_ALL=C fdisk ${BOOTDRIVE} > /dev/null 2>&1
-fi
-
-sudo mkfs.${FSTYPE} ${MK_OPTS} -L "$DISKLABEL-RW" "${BOOTDRIVE}2" || oops "mkfs failed"
-
+SUBSCRIPT=/tmp/mountandcopy.sh
 T=/tmp/plop_storage_creation
+cat > $SUBSCRIPT <<EOF
+oops() {
+	echo "Error ! $*"
+	exit -1
+}
+partx "$BOOTDRIVE"
+if [ "$PARTCOUNT" -ne "3" ]; then
+	echo "Invalid partition layout"
+	exit 200
+fi
+# extend partition
+echo "d\n2\nn\np\n\n\n\n\nw" | LC_ALL=C fdisk ${BOOTDRIVE} > /dev/null 2>&1
+
+# make fs
+mkfs.${FSTYPE} ${MK_OPTS} -L "$DISKLABEL-RW" "${BOOTDRIVE}2" || oops "mkfs failed"
+
+# copy files
 mkdir "$T"
-sudo mount -o ${M_OPTS} "${BOOTDRIVE}2" "$T" || oops "mount failed"
-sudo tar xf "$R/boot/rootfs.default" -C "$T" || oops "unpack failed"
-sudo umount "$T"
+mount -o ${M_OPTS} "${BOOTDRIVE}2" "$T" || oops "mount failed"
+tar xf "$R/boot/rootfs.default" -C "$T" || oops "unpack failed"
+umount "$T"
 rmdir "$T"
-sudo sync
 sync
 sync
 sync
-sudo partx "$BOOTDRIVE"
+sync
+partx "$BOOTDRIVE"
+EOF
+pkexec /bin/sh $SUBSCRIPT
+rm $SUBSCRIPT
 echo "$FSTYPE FS BUILT"
-echo "---------------------------"
-echo " Reboot to enjoy changes !"
-echo "---------------------------"
 zenity --warning --text "Now reboot to apply changes"
