1c8425c8389db2a2942844bbc2b93fb026fbdf4f
fab <fdev31@gmail.com>
2016-02-23 02:45:35 +0100

    add pulse additions to snd-more

---
+++ hooks/snd-more/install/50_pulse_more.sh
@@ -0,0 +1,10 @@
+install_pkg pulseaudio-bluetooth
+install_pkg pulseaudio-equalizer
+install_pkg pulseaudio-zeroconf
+if have_xorg ; then
+    install_pkg paman
+    install_pkg pavucontrol
+    install_pkg pavumeter
+    install_pkg paprefs
+    install_pkg pasystray
+fi
