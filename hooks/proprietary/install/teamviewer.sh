b45e7ea859cd0e5bbb283723fbaca8df146f4411
fab <fdev31@gmail.com>
2016-02-23 18:03:59 +0100

    start teamviewer daemon on demand

--- hooks/proprietary/install/teamviewer.sh
+++ hooks/proprietary/install/teamviewer.sh
@@ -1,2 +1,10 @@
 install_pkg teamviewer
+
+echo "#!/bin/sh
+if ! systemctl list-units |grep ^teamviewerd | grep running ; then
+    systemctl start teamviewerd
+fi
+teamviewer
+" | sudo dd of="$R/bin/runteamviewer.sh"
+sudo chmod 755 "$R/bin/runteamviewer.sh"
 #enable_service teamviewerd
