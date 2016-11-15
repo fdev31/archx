7ae75c92c82d7345436ac8770b12e7ca671d13c7
fab <fdev31@gmail.com>
2016-11-15 11:34:02 +0100

    update teamviewer install

--- hooks/proprietary/install/teamviewer.sh
+++ hooks/proprietary/install/teamviewer.sh
@@ -7,4 +7,12 @@ fi
 teamviewer
 " | sudo dd of="$R/bin/runteamviewer.sh"
 sudo chmod 755 "$R/bin/runteamviewer.sh"
+
+echo "#!/bin/sh
+if ! systemctl list-units |grep ^teamviewerd | grep running ; then
+    systemctl start teamviewerd
+fi
+teamviewer
+" | sudo dd of="$R/bin/runteamviewer.sh"
+sudo chmod 755 "$R/bin/runteamviewer.sh"
 #enable_service teamviewerd
