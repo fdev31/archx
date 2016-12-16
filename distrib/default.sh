5acbd1b97d53c24201f0f982af27005ab96b3f18
fab <fdev31@gmail.com>
2016-12-16 23:44:52 +0100

    add cinnamon to default distrib

--- distrib/default.sh
+++ distrib/default.sh
@@ -1,4 +1,4 @@
-PROFILES='base xorg env-gnome net-utils system snd-base snd-more chat gfx-base gfx-more photo-base office-base office-more locales medical proprietary dev-base installer'
+PROFILES='base xorg env-gnome env-cinnamon net-utils system snd-base snd-more chat gfx-base gfx-more photo-base office-base office-more locales medical proprietary dev-base installer'
 PREFERRED_TOOLKIT=gtk
 DISTRO_PACKAGE_LIST='firefox-ublock-origin firefox-download-youtube-videos-as-mp4'
 function distro_install_hook() {
