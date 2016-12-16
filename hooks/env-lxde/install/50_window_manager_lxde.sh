83fe21b7486d9ad795a7916c2515b37775a84957
fab <fdev31@gmail.com>
2016-12-17 00:09:33 +0100

    add a few environments

---
+++ hooks/env-lxde/install/50_window_manager_lxde.sh
@@ -0,0 +1,1 @@
+../../alternatives/install/50_window_manager_lxde.sh
---
+++ hooks/env-pantheon/install/80_pantheon_desktop.sh
@@ -0,0 +1,6 @@
+install_pkg audience
+install_pkg contractor
+install_pkg noise-player
+install_pkg pantheon-files
+install_pkg pantheon-photos
+install_pkg pantheon-terminal
