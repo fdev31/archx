5fc1f03b4812b37c851c4e975c6ad2840b43ae77
fab <fdev31@gmail.com>
2016-12-25 01:21:35 +0100

    default now uses groups + install xfce by default

--- distrib/default.sh
+++ distrib/default.sh
@@ -1,6 +1,7 @@
-PROFILES='base xorg env-gnome env-cinnamon net-utils system snd-base snd-more chat gfx-base gfx-more photo-base office-base office-more locales medical proprietary dev-base installer'
+PROFILES="$PKG_BASE $PKG_XORG $PKG_EDIT $PKG_GFX $PKG_UI $PKG_EMU $PKG_UI $PKG_EMU $PKG_DOC $PKG_SND proprietary env-xfce"
 PREFERRED_TOOLKIT=gtk
 DISTRO_PACKAGE_LIST='firefox-ublock-origin firefox-download-youtube-videos-as-mp4'
 function distro_install_hook() {
     return
  }
+
