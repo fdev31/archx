594b411018517730b2c7dd879f821784e2ba4ee6
fab <fdev31@gmail.com>
2017-01-07 16:10:51 +0100

    grub: change font & background

--- hooks/base/install/90_grub_conf.sh
+++ hooks/base/install/90_grub_conf.sh
@@ -3,6 +3,7 @@ install_pkg grub
 sudo mkdir -p "$R/boot/grub/fonts/" 2> /dev/null
 sudo mkdir -p "$R/boot/grub/themes/" 2> /dev/null
 sudo cp -r "$R/usr/share/grub/"*.pf2 "$R/boot/grub/fonts/"
+sudo cp -r resources/*.pf2 "$R/boot/grub/fonts/"
 sudo cp -r resources/breeze "$R/boot/grub/themes/"
 install_file resources/grub.cfg "/boot/grub/grub.cfg"
 sudo sed -i "s/DISKLABEL/$DISKLABEL/g" "$R/boot/grub/grub.cfg"
