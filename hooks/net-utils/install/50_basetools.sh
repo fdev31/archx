f7534f5863f22bd54b6a12c90d539415739e3ac1
fab <fdev31@gmail.com>
2017-01-11 18:32:43 +0100

    add mdns support

--- hooks/net-utils/install/50_basetools.sh
+++ hooks/net-utils/install/50_basetools.sh
@@ -1,4 +1,6 @@
 install_pkg whois iputils netcat lftp avahi nss-mdns
 have_xorg && install_pkg wireshark-$PREFERRED_TOOLKIT
 
+enable_service avahi-daemon
 enable_service avahi-dnsconfd
+install_file resources/nsswitch.conf "/etc/nsswitch.conf"
