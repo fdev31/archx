87d03465754e9d34b10fd2d980f72b9894a3cfc4
fab <fdev31@gmail.com>
2017-01-12 14:24:42 +0100

    update themes

--- hooks/lookandfeel/install/default-theme.sh
+++ hooks/lookandfeel/install/default-theme.sh
@@ -1,4 +1,5 @@
 install_pkg arc-gtk-theme
-install_pkg xcursor-archcursorblue
-install_pkg sardi-icons
 install_pkg vertex-themes
+install_pkg xcursor-archcursorblue
+install_pkg xcursor-pinux
+install_pkg sardi-icons
