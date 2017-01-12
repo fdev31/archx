14a3c4a1e249a2b1af84d80dc70340f654b1e01d
fab <fdev31@gmail.com>
2017-01-12 20:23:46 +0100

    add a script to build all distros

---
+++ build_all_distros.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. configuration.sh
+
+for n in distrib/*.sh; do
+    n=${n##*/}
+    n=${n%.sh}
+    echo "DISTRIB=$n" > my_conf.sh
+    sudo rm -fr "$R"
+    ./mkbootstrap.sh
+    mv ARCHX.img ARCHX-$n.img
+done
