fe72c47f551d04e61ac36ddded81d2520065251d
fab <fdev31@gmail.com>
2017-01-12 19:44:45 +0100

    fix enlightenment installation

--- hooks/alternatives/install/50_window_manager_enlightenment.sh
+++ hooks/alternatives/install/50_window_manager_enlightenment.sh
@@ -1,1 +1,6 @@
-install_pkg  enlightenment epplet-base elementary
+install_pkg enlightenment
+install_pkg epplet-base
+install_pkg econnman
+install_pkg elementary-icon-theme
+
+network_manager connman
