5cc5643b80ac15eebc3831f0ddf4075e2f910c1d
fab <fdev31@gmail.com>
2017-01-12 19:44:25 +0100

    chose default archiver for awesome

--- hooks/env-awesome/install/50_pcmanfm.sh
+++ hooks/env-awesome/install/50_pcmanfm.sh
@@ -1,7 +1,5 @@
 install_pkg evince-thumbnailer
-install_pkg file-roller
-install_pkg engrampa
-install_pkg xarchiver
-install_pkg libgsf
 install_pkg ffmpegthumbnailer
+install_pkg engrampa
+install_pkg libgsf
 install_pkg pcmanfm 
