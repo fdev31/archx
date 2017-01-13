cbc70bdc897e1df13cc2f41dbfd5a9ed36ca4b37
fab <fdev31@gmail.com>
2017-01-13 21:46:41 +0100

    fix kde isntalls

--- hooks/system/install/90_package_manager.sh
+++ hooks/system/install/90_package_manager.sh
@@ -1,9 +1,11 @@
 if have_xorg ; then
     if [ "$PREFERRED_TOOLKIT" = "qt" ]; then
         install_pkg packagekit-qt5
-        install_pkg octopi
+        install_pkg plasma-pk-updates-git
+        install_pkg apper
+#        install_pkg octopi # may replace pamac-aur
     else
         install_pkg gnome-packagekit
-        install_pkg pamac-aur
     fi
 fi
+install_pkg pamac-aur
