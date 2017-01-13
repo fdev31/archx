725f85591d30010d73ffd710f3c76c3f77f1d068
fab <fdev31@gmail.com>
2017-01-13 21:47:00 +0100

    don't install gtk themes with qt config

--- hooks/lookandfeel/install/default-theme.sh
+++ hooks/lookandfeel/install/default-theme.sh
@@ -1,5 +1,12 @@
-install_pkg arc-gtk-theme
-install_pkg vertex-themes
-install_pkg xcursor-archcursorblue
-install_pkg xcursor-pinux
-install_pkg sardi-icons
+if have_xorg ; then
+    if [ "$PREFERRED_TOOLKIT" = "qt" ]; then
+        install_pkg qtcurve
+        install_pkg archlinux-themes-kdm
+    else
+        install_pkg arc-gtk-theme
+        install_pkg vertex-themes
+        install_pkg xcursor-archcursorblue
+        install_pkg xcursor-pinux
+        install_pkg sardi-icons
+    fi
+fi
