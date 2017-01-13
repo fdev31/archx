ab9dc26e9946d930b9066d8915c15b89159d0cd5
fab <fdev31@gmail.com>
2017-01-13 15:10:30 +0100

    paperwork build is broken (too many unsafe dependencies)

--- hooks/office-base/install/90_paperwork.sh
+++
@@ -1,6 +0,0 @@
-install_pkg tesseract-data-${LANG_ISO3}
-l=$(LC_ALL=C raw_install_pkg -Si paperwork |grep "^Depends")
-for d in ${l#*: }; do
-    install_pkg --asdeps "$d"
-done
-install_pkg paperwork
