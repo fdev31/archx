ffdbb80f114df730c0ee1f85f572ef1dfcd5bba7
fab <fdev31@gmail.com>
2017-01-13 21:46:28 +0100

    update distros

---
+++ distrib/noobs.sh
@@ -0,0 +1,7 @@
+PROFILES="$PKG_BASE $PKG_XORG env-awesome env-enlightenment noobs" 
+
+PREFERRED_TOOLKIT='gtk'
+
+function distro_install_hook() {
+    return
+}
--- distrib/plasma.sh
+++ distrib/plasma.sh
@@ -1,8 +1,8 @@
-PROFILES="$PKG_BASE $PKG_XORG $PKG_DOC env-kde"
+PROFILES="$PKG_BASE $PKG_XORG env-kde"
 
 PREFERRED_TOOLKIT='qt'
 
-DISTRO_PACKAGE_LIST='amarok calligra digiKam kde-applications kde-multimedia kde-edu kde-graphics'
+#DISTRO_PACKAGE_LIST='amarok calligra digiKam kde-applications kde-multimedia kde-edu kde-graphics'
 
 function distro_install_hook() {
     return
