cde28769813a6b281121514fd34a284299ead88c
fab <fdev31@gmail.com>
2017-01-16 17:11:08 +0100

    copy archlinux installer on install

--- hooks/installer/install/50_install_scripts.sh
+++ hooks/installer/install/50_install_scripts.sh
@@ -2,6 +2,7 @@ install_pkg arch-install-scripts
 install_bin resources/installer.py /bin/
 install_bin resources/mkparts.sh /bin/installer-standard.sh
 install_bin resources/installhere.sh /bin/installer-embed.sh
+install_bin resources/mkarch.sh /bin/installer-archlinux.sh
 sudo mkdir -p "$R/usr/share/installer"
 install_bin resources/instlib.sh /usr/share/installer
 install_pkg parted # used for partprobe
