4f9ae0d97eae865e3d2ae72e2f5d8b537c081bca
fab <fdev31@gmail.com>
2017-01-16 22:21:21 +0100

    clean up locales management

--- resources/locales/gettext/refresh.sh
+++ resources/locales/gettext/refresh.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xgettext translations.py
+TEMPLATE=messages.pot
+xgettext translations.py --output $TEMPLATE
+for pofile in */LC_MESSAGES/messages.po ; do
+    msgmerge --update --no-fuzzy-matching --backup=off $pofile $TEMPLATE
+    msgfmt $pofile --output-file ${pofile%.po}.mo
+done
