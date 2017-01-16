8fae69cd5f4e4f329759747dd37bccc0f84dab34
fab <fdev31@gmail.com>
2017-01-17 00:44:19 +0100

    don't set useless value

--- distrib/console.sh
+++ distrib/console.sh
@@ -1,7 +1,6 @@
 PROFILES="base locales installer" # system installer
 
 DISK_MARGIN=50 # extra space for persistence, also HOME size, used for loopback or disk images
-BOOT_MARGIN=50 # extra space for /boot (first partition)
 BOOT_TARGET="multi-user"
 
 function distro_install_hook() {
