a6195d6ee4c16eed631995f789831da9ac52137c
fab <fdev31@gmail.com>
2017-01-16 12:55:03 +0100

    wps: also installs dictionary

--- hooks/office-base/install/90_wpsoffice.sh
+++ hooks/office-base/install/90_wpsoffice.sh
@@ -1,3 +1,6 @@
-have_xorg && install_pkg ttf-wps-fonts
-have_xorg && install_pkg ttf-wps-win10
-have_xorg && install_pkg wps-office
+if have_xorg ; then
+    install_pkg wps-office-extension-${LANG_FULL}-dictionary
+    install_pkg ttf-wps-fonts
+    install_pkg ttf-wps-win10
+    install_pkg wps-office
+fi
