98e54bcba8a2c3f6b6612bd1757f70eb82b72e60
fab <fdev31@gmail.com>
2017-01-19 00:56:11 +0100

    don't rely on dbus tools to set locales :P

--- hooks/base/post-install/10_locales.sh
+++ hooks/base/post-install/10_locales.sh
@@ -1,6 +1,5 @@
 echo "${LANG} UTF-8" | sudo dd of="$R/etc/locale.gen"
 sudo arch-chroot "$R" locale-gen
-set-locale LANG=${LANG} LC_NUMERIC=C
 
 (cd "$R/etc" && sudo ln -sf "/usr/share/zoneinfo/${LANG_TZ}" localtime)
 echo "
@@ -18,4 +17,11 @@ LC_MEASUREMENT=\"${LANG}\"
 LC_IDENTIFICATION=\"${LANG}\"
 LC_NUMERIC=\"C\"" | sudo dd of="$R/etc/locale.conf"
 
-sudo arch-chroot "$R" localectl set-x11-keymap ${LANG_ISO2} '' '' terminate:ctrl_alt_bksp
+echo "
+Section \"InputClass\"
+    Identifier \"system-keyboard\"
+    MatchIsKeyboard \"on\"
+    Option \"XkbLayout\" \"${LANG_ISO2}\"
+    Option \"XkbOptions\" \"terminate:ctrl_alt_bksp\"
+EndSection
+" | sudo dd of="$R/etc/X11/xorg.conf.d/00-keyboard.conf"
