baa645a26701bb80622690344fe64030968093fd
fab <fdev31@gmail.com>
2017-01-19 17:04:41 +0100

    remove applications

--- hooks/alternatives/install/50_window_manager_cinnamon.sh
+++ hooks/alternatives/install/50_window_manager_cinnamon.sh
@@ -1,19 +1,18 @@
+install_pkg polkit-gnome
+
 install_pkg cinnamon
-install_pkg cinnamon-control-center
-install_pkg cinnamon-desktop
 install_pkg cinnamon-menus
-install_pkg cinnamon-screensaver
 install_pkg cinnamon-session
+install_pkg cinnamon-desktop
+install_pkg cinnamon-screensaver
 install_pkg cinnamon-translations
+install_menu cinnamon-applications
+install_pkg cinnamon-control-center
 
 install_pkg nemo
 install_pkg nemo-fileroller
 install_pkg nemo-preview
 install_pkg nemo-share
-install_pkg polkit-gnome
-
-install_menu cinnamon-applications
-install_application '*'
 
 # extra
 install_pkg shutter
