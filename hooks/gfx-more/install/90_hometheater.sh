92353b708c0debfd0e476d95d78452733c81759f
fab <fdev31@gmail.com>
2017-01-19 15:32:04 +0100

    split kodi to a separate (deactivated) package

--- hooks/gfx-more/install/90_hometheater.sh
+++ hooks/gfx-more/install/90_hometheater.sh
@@ -1,11 +1,2 @@
-# alternative: openpht
-install_pkg kodi
-install_pkg kodi-audioencoder-vorbis kodi-audioencoder-lame
-
-echo "needs_root_rights = yes" | sudo dd of="$R/etc/X11/Xwrapper.config"
-
-sudo useradd --system -R "$R" -G $DEFAULT_GROUPS -m kodiuser
-
 have_xorg && install_pkg streamstudio-bin
 have_xorg && install_pkg popcorntime-ce-bin
-
