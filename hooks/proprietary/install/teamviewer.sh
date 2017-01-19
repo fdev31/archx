8e8d330b65c8e742474bf07094b3f20a67a7af34
fab <fdev31@gmail.com>
2017-01-19 14:04:27 +0100

    change teamviewer installation a bit

--- hooks/proprietary/install/teamviewer.sh
+++ hooks/proprietary/install/teamviewer.sh
@@ -1,20 +1,12 @@
 if have_xorg ; then
     install_pkg teamviewer
-
     echo "#!/bin/sh
     if ! systemctl list-units |grep ^teamviewerd | grep running ; then
         systemctl start teamviewerd
+        sleep 1
     fi
-    teamviewer
+    exec teamviewer
     " | sudo dd of="$R/bin/runteamviewer.sh"
     sudo chmod 755 "$R/bin/runteamviewer.sh"
-
-    echo "#!/bin/sh
-    if ! systemctl list-units |grep ^teamviewerd | grep running ; then
-        systemctl start teamviewerd
-    fi
-    teamviewer
-    " | sudo dd of="$R/bin/runteamviewer.sh"
-    sudo chmod 755 "$R/bin/runteamviewer.sh"
-    enable_service teamviewerd
+#    enable_service teamviewerd
 fi
