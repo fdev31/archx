e4a589c22f439dddd20c148aa8a1ac2f8e76a662
fab <fdev31@gmail.com>
2017-01-19 15:27:41 +0100

    add mate by default

--- distrib/default.sh
+++ distrib/default.sh
@@ -1,6 +1,8 @@
-PROFILES="$PKG_ALL env-awesome env-gnome"
+PROFILES="$PKG_ALL env-awesome env-gnome env-mate"
 PREFERRED_TOOLKIT=gtk
 DISTRO_PACKAGE_LIST='firefox-ublock-origin firefox-download-youtube-videos-as-mp4 dropbox deluge handbrake vlc firefox-flashgot'
+DISK_TOTAL_SIZE=4
+
 function distro_install_hook() {
     sudo sed -i "s/^#user-session=default.*/user-session=gnome/" "$R/etc/lightdm/lightdm.conf"
 
