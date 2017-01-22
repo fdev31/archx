8ef6de514f69b29a162faf348746f4002683f768
fab <fdev31@gmail.com>
2017-01-22 16:04:26 +0100

    add a few more packages to MATE desktop

--- hooks/alternatives/install/50_window_manager_mate.sh
+++ hooks/alternatives/install/50_window_manager_mate.sh
@@ -1,5 +1,16 @@
 install_pkg mate-gtk3
-install_pkg mate-extra-gtk3
-install_pkg mate-netbook-gtk3
+c
+
+iadd a few more packages to MATE desktophg diff .
+hg ci b/hooks/alternatives/install/50_window_manager_mate.shc
+
 install_pkg mate-applet-streamer
+
+install_pkg caja-actions-gtk3
+
+install_pkg mate-tweak
+install_pkg plank
+install_pkg plank-config
+install_pkg tilda
+
 install_pkg variety
