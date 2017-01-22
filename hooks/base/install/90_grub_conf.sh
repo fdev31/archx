1c8c7309de063f93e6f0dc4f0f35727c2eb5a172
fab <fdev31@gmail.com>
2017-01-22 12:44:35 +0100

    move grub stuff to a subfolder

--- hooks/base/install/90_grub_conf.sh
+++ hooks/base/install/90_grub_conf.sh
@@ -3,9 +3,9 @@ install_pkg grub
 sudo mkdir -p "$R/boot/grub/fonts/" 2> /dev/null
 sudo mkdir -p "$R/boot/grub/themes/" 2> /dev/null
 install_file "$R/usr/share/grub/"*.pf2 "/boot/grub/fonts/" 
-install_file resources/*.pf2 "/boot/grub/fonts/"
+install_file resources/grub/*.pf2 "/boot/grub/fonts/"
 sudo cp -r resources/breeze "$R/boot/grub/themes/"
-install_file resources/grub.cfg "/boot/grub/grub.cfg"
+install_file resources/grub/grub.cfg "/boot/grub/grub.cfg"
 sudo sed -i "s/DISKLABEL/$DISKLABEL/g" "$R/boot/grub/grub.cfg"
 
 echo 'GRUB_THEME="/boot/grub/themes/breeze/theme.txt"' | append_text "/etc/default/grub"
