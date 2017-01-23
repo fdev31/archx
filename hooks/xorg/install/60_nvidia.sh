0d6c72f1d863faf4a9a05fca4abde35a0699ee65
fab <fdev31@gmail.com>
2017-01-23 14:34:58 +0100

    add more packages to nvidia installation

--- hooks/xorg/install/60_nvidia.sh
+++ hooks/xorg/install/60_nvidia.sh
@@ -1,7 +1,11 @@
 install_pkg nvidia
 install_pkg nvidia-libgl
-install_pkg lib32-nvidia-libgl
+install_pkg mesa-vdpau
+install_pkg libva-vdpau-driver
 install_pkg opencl-nvidia
+install_pkg lib32-nvidia-libgl
+install_pkg nvidia-utils
+#install_pkg cuda
 
 if [ ! -f "$R/etc/modprobe.d/nvidia.conf" ]
 then
