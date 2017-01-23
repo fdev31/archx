a0539f15359b4fbc6419d452f009c275877f8887
fab <fdev31@gmail.com>
2017-01-23 23:31:08 +0100

    merge nvidia hook into xorg hook

--- hooks/xorg/install/20_xorg.sh
+++ hooks/xorg/install/20_xorg.sh
@@ -9,11 +9,27 @@ if [ "$PREFERRED_TOOLKIT" = "gtk" ]; then
     install_pkg gnome-vfs
 fi
 
-install_pkg libx264 mesa-libgl
-install_pkg mesa-libgl
-install_pkg lib32-mesa-libgl
-install_pkg libva-vdpau-driver
-install_pkg libva-intel-driver
-install_pkg mesa-vdpau
-install_pkg libvdpau
+install_pkg libx264
 install_pkg gnome-keyring
+if [ -z "$USE_NVIDIA" ]; then
+    install_pkg mesa-libgl
+    install_pkg lib32-mesa-libgl
+    install_pkg libva-vdpau-driver
+    install_pkg libva-intel-driver
+    install_pkg mesa-vdpau
+    install_pkg libvdpau
+else
+    install_pkg nvidia
+    install_pkg nvidia-libgl
+    install_pkg mesa-vdpau
+    install_pkg libva-vdpau-driver
+    install_pkg opencl-nvidia
+    install_pkg lib32-nvidia-libgl
+    install_pkg nvidia-utils
+    #install_pkg cuda
+
+    if [ ! -f "$R/etc/modprobe.d/nvidia.conf" ]
+    then
+        echo "blacklist nouveau" | append_text "/etc/modprobe.d/nvidia.conf"
+    fi
+fi
