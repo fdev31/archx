a4973dd15dc55563e7e0cf1fd99f6eb8f5665519
fab <fdev31@gmail.com>
2017-01-23 13:10:04 +0100

    make pantheon repositories conditional

--- hooks/env-pantheon/install/50_pantheon_desktop.sh
+++ hooks/env-pantheon/install/50_pantheon_desktop.sh
@@ -1,3 +1,9 @@
+append_text "/etc/pacman.conf" <<EOF
+[pantheon]
+SigLevel = Optional
+Server = http://pkgbuild.com/~alucryd/\$repo/\$arch
+EOF
+
 install_pkg pantheon
 install_pkg pantheon-files
 install_pkg pantheon-photos
