984321cb5af291296a2753258d713064d0d38441
fab <fdev31@gmail.com>
2017-02-10 02:05:36 +0100

    few improvements to dm config

--- hooks/alternatives/install/login_manager/40_login_manager_lxdm.sh
+++ hooks/alternatives/install/login_manager/40_login_manager_lxdm.sh
@@ -1,6 +1,8 @@
 install_pkg lxdm
 install_pkg archlinux-lxdm-theme-full
-sudo sed -i "s#/usr/bin/startlxde#/bin/$ENV-session#" "$R/etc/lxdm/lxdm.conf"
+sudo sed -i "s#^.*autologin=.*#autologin=user#" "$R/etc/lxdm/lxdm.conf"
+sudo sed -i "s#^.*timeout=.*#timeout=5#" "$R/etc/lxdm/lxdm.conf"
+sudo sed -i "s#^.*/usr/bin/startlxde#session=/bin/$ENV-session#" "$R/etc/lxdm/lxdm.conf"
 sudo sed -i "s#Adwaita#Breeze#" "$R/etc/lxdm/lxdm.conf"
 sudo sed -i "s#Industrial#Archlinux#" "$R/etc/lxdm/lxdm.conf"
 enable_service lxdm
