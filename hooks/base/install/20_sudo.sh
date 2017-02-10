5a566a9cb9bb450381ec5680f72b03a369b5c29d
fab <fdev31@gmail.com>
2017-02-10 15:47:17 +0100

    disable password during install

--- hooks/base/install/20_sudo.sh
+++ hooks/base/install/20_sudo.sh
@@ -1,3 +1,3 @@
 install_pkg  sudo
 
-sudo install -TD -o root -g root -m 660 resources/sudo_conf "$R/etc/sudoers.d/50_nopassword"
+sudo install -TD -o root -g root -m 660 resources/sudo_conf_nopass "$R/etc/sudoers.d/50_nopassword"
---
+++ hooks/base/install/90_sudo.sh
@@ -0,0 +1,1 @@
+sudo install -TD -o root -g root -m 660 resources/sudo_conf "$R/etc/sudoers.d/50_nopassword"
