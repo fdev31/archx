62dec19d9f7a434f4ec5efbe021e3f99b56932c9
fab <fdev31@gmail.com>
2017-02-10 02:23:13 +0100

    teamviewer hook refactor + fix using gksu

--- hooks/proprietary/install/50_teamviewer.sh
+++ hooks/proprietary/install/50_teamviewer.sh
@@ -1,12 +1,12 @@
 if have_xorg ; then
     install_pkg teamviewer
-    echo "#!/bin/sh
-    if ! systemctl list-units |grep ^teamviewerd | grep running ; then
-        systemctl start teamviewerd
-        sleep 1
-    fi
-    exec teamviewer
-    " | sudo dd of="$R/bin/runteamviewer.sh"
-    sudo chmod 755 "$R/bin/runteamviewer.sh"
+    write_bin /bin/runteamviewer.sh << EOF
+echo "#!/bin/sh
+if ! systemctl list-units |grep ^teamviewerd | grep running ; then
+    gksu systemctl start teamviewerd
+    sleep 3
+fi
+exec teamviewer
+EOF
 #    enable_service teamviewerd
 fi
