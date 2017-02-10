96158cd860bf29e4dec02e1aec116791ed662343
fab <fdev31@gmail.com>
2017-02-10 15:48:11 +0100

    mate explicitely uses aur to install tweaks + session fix in lxdm

--- hooks/alternatives/install/50_window_manager_mate.sh
+++ hooks/alternatives/install/50_window_manager_mate.sh
@@ -3,5 +3,6 @@ install_pkg mate-extra-gtk3
 #install_pkg mate-netbook-gtk3
 install_pkg mate-applet-streamer
 install_pkg caja-actions-gtk3
-install_pkg mate-tweak
+install_pkg mate-applets-gtk3
+install_aur_pkg mate-tweak
 install_pkg plank
