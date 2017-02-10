d3ed2b4130abf937ea07f1eacf4401d9f412ce2b
fab <fdev31@gmail.com>
2017-02-10 02:34:09 +0100

    move some lightdm specific code

--- distrib/default.sh
+++ distrib/default.sh
@@ -5,21 +5,15 @@ DISTRO_PACKAGE_LIST=
 DISK_TOTAL_SIZE=4
 
 function distro_install_hook() {
-    sudo sed -i "s/^#user-session=.*/user-session=$ENV/" "$R/etc/lightdm/lightdm.conf"
+    echo "Writing $R/home/$USERNAME/.dmrc"
 
-    # comment out to disable autologin
-    sudo sed -i "s/^#autologin-user=.*/autologin-user=user/" "$R/etc/lightdm/lightdm.conf"
-    sudo sed -i "s/^#autologin-user-timeout=.*/autologin-user-timeout=10/" "$R/etc/lightdm/lightdm.conf"
-    sudo sed -i "s/^#autologin-in-background=.*/autologin-in-background=true/" "$R/etc/lightdm/lightdm.conf"
-
-    sudo groupadd -R "$R" -r autologin
-    sudo gpasswd  -Q "$R" -a user autologin
-
-    cat > $R/home/$USERNAME/.dmrc <<EOF 
+    cat > "$R/home/$USERNAME/.dmrc" <<EOF 
 [Desktop]
 Language=$LANG
 Session=$ENV
 EOF
+    sudo groupadd -R "$R" -r autologin
+    sudo gpasswd  -Q "$R" -a user autologin
 
     return
 }
--- hooks/alternatives/install/login_manager/40_login_manager_lightdm.sh
+++ hooks/alternatives/install/login_manager/40_login_manager_lightdm.sh
@@ -10,3 +10,10 @@ sudo mkdir "$R/run/lightdm" 2> /dev/null # fix warning
 install_pkg  lightdm
 
 enable_service lightdm
+
+sudo sed -i "s/^#user-session=.*/user-session=$ENV/" "$R/etc/lightdm/lightdm.conf"
+
+# comment out to disable autologin
+sudo sed -i "s/^#autologin-user=.*/autologin-user=user/" "$R/etc/lightdm/lightdm.conf"
+sudo sed -i "s/^#autologin-user-timeout=.*/autologin-user-timeout=10/" "$R/etc/lightdm/lightdm.conf"
+sudo sed -i "s/^#autologin-in-background=.*/autologin-in-background=true/" "$R/etc/lightdm/lightdm.conf"
