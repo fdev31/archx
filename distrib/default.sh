52b334a24303d010c5d9e0647f68dcce47765f53
fab <fdev31@gmail.com>
2017-02-15 17:26:09 +0100

    add minimal gnome desktop to default

--- distrib/default.sh
+++ distrib/default.sh
@@ -1,6 +1,6 @@
 ENV=mate # mate or gnome
 NETMGR=networkmanager
-PROFILES="$PKG_ALL env-$ENV env-$ENV-apps env-awesome"
+PROFILES="$PKG_ALL env-$ENV env-$ENV-apps env-awesome env-gnome"
 PREFERRED_TOOLKIT=gtk
 DISTRO_PACKAGE_LIST=
 DISK_TOTAL_SIZE=4
