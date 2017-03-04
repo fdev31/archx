e8e07d336ee270d352669321978aeebf9eb3985a
fab <fdev31@gmail.com>
2017-03-04 01:57:36 +0100

    set numlock in lxdm

--- hooks/alternatives/install/login_manager/40_login_manager_lxdm.sh
+++ hooks/alternatives/install/login_manager/40_login_manager_lxdm.sh
@@ -3,6 +3,8 @@ install_pkg archlinux-lxdm-theme-full
 sudo sed -i "s#^.*autologin=.*#autologin=user#" "$R/etc/lxdm/lxdm.conf"
 sudo sed -i "s#^.*timeout=.*#timeout=5#" "$R/etc/lxdm/lxdm.conf"
 sudo sed -i "s@# session=.*@session=/bin/$ENV-session@" "$R/etc/lxdm/lxdm.conf"
+sudo sed -i "s@# numlock=0@numlock=1@" "$R/etc/lxdm/lxdm.conf"
 sudo sed -i "s#Adwaita#Breeze#" "$R/etc/lxdm/lxdm.conf"
 sudo sed -i "s#Industrial#Archlinux#" "$R/etc/lxdm/lxdm.conf"
+
 enable_service lxdm
