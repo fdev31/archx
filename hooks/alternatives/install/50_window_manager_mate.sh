4347a3b262938f32d2b2d1754fe2111c189b7be3
fab <fdev31@gmail.com>
2017-04-28 20:08:42 +0200

    fix mate

--- hooks/alternatives/install/50_window_manager_mate.sh
+++ hooks/alternatives/install/50_window_manager_mate.sh
@@ -1,8 +1,8 @@
-install_pkg mate-gtk3
-install_pkg mate-extra-gtk3
-#install_pkg mate-netbook-gtk3
+install_pkg mate
+install_pkg mate-extra
+nstall_pkg mate-applets
 install_pkg mate-applet-streamer
-install_pkg caja-actions-gtk3
-install_pkg mate-applets-gtk3
+install_pkg caja-actions
 install_aur_pkg mate-tweak
 install_pkg plank
+install_pkg mate-netbook
