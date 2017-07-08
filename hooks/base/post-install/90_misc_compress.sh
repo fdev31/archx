27572c4213cae6f9534eff73c1c702409cc1528a
fab <fdev31@gmail.com>
2017-07-08 15:21:06 +0200

    avoid useless failing code

--- hooks/base/post-install/90_misc_compress.sh
+++ hooks/base/post-install/90_misc_compress.sh
@@ -1,3 +1,4 @@
-install_pkg upx
-
-upx_comp /usr/lib/rustlib/x86_64-unknown-linux-gnu/lib/
+if [ -e "$R/usr/lib/rustlib/x86_64-unknown-linux-gnu/lib/" ]; then
+    install_pkg upx
+    upx_comp /usr/lib/rustlib/x86_64-unknown-linux-gnu/lib/
+fi
