5952aba646d9a5d2b05d517e25698672645a7ef0
fab <fdev31@gmail.com>
2017-07-09 01:03:20 +0200

    recover missing initcpio conf

--- hooks/flashdisk/pre-mkinitcpio/50_mkinitcpio_conf.sh
+++ hooks/flashdisk/pre-mkinitcpio/50_mkinitcpio_conf.sh
@@ -3,3 +3,5 @@ MODULES='squashfs vfat loop overlay btrfs ext4 ntfs'
 HOOKS='base udev keyboard block rolinux shutdown'
 COMPRESSION='$COMPRESSION_TYPE'
 EOF
+
+install_file resources/linux.preset "/etc/mkinitcpio.d/linux.preset"
