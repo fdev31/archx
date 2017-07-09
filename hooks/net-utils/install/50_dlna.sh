c169a22e5be4c7d470538fab437cfc24306a4791
fab <fdev31@gmail.com>
2017-07-09 04:27:20 +0200

    add eezupnp

--- hooks/net-utils/install/50_dlna.sh
+++ hooks/net-utils/install/50_dlna.sh
@@ -1,6 +1,7 @@
 install_pkg --asdeps gst-libav
 install_pkg --asdeps tracker
 install_pkg rygel
+install_pkg eezupnp
 if have_xorg ; then # totem client
    install_pkg dleyna-server
    install_pkg gupnp-av
