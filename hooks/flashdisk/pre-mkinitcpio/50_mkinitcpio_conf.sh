7cc66d330843bd4cd72a603d4f14cef020f1cc37
fab <fdev31@gmail.com>
2017-11-08 21:40:16 +0100

    use new array configuration for mkinitcpio

--- hooks/flashdisk/pre-mkinitcpio/50_mkinitcpio_conf.sh
+++ hooks/flashdisk/pre-mkinitcpio/50_mkinitcpio_conf.sh
@@ -1,6 +1,6 @@
 append_text "/etc/mkinitcpio.conf" <<EOF
-MODULES='squashfs vfat loop overlay btrfs ext4 ntfs'
-HOOKS='base udev keyboard block rolinux shutdown'
+MODULES=(squashfs vfat loop overlay btrfs ext4 ntfs)
+HOOKS=(base udev keyboard block rolinux shutdown)
 COMPRESSION='$COMPRESSION_TYPE'
 EOF
 
