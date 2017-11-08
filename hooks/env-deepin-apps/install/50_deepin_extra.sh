18105881b9f288ea0687169b128f5ec5265f857e
fab <fdev31@gmail.com>
2017-11-08 21:57:06 +0100

    split deeping in two packages (wm & apps)

---
+++ hooks/env-deepin-apps/install/50_deepin_extra.sh
@@ -0,0 +1,8 @@
+#remove_pkg cogl # fix for cogl-deepin conflict
+install_pkg deepin-extra
+install_pkg deepin-game
+install_pkg deepin-movie
+install_pkg deepin-music
+install_pkg deepin-screenshot
+install_pkg deepin-terminal
+install_pkg deepin-gtk-theme
