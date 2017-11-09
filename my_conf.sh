656dde264ce685cc0f406af5b363e24dac91a66f
fab <fdev31@gmail.com>
2017-11-09 20:48:11 +0100

    minor update to default conf

--- my_conf.sh
+++ my_conf.sh
@@ -1,8 +1,7 @@
 #  Uncomment values and set to your own taste
 #  This overrides the variables found in configuration.sh
-
 #COMPRESSION_TYPE=gzip
-#PACMAN_BIN=pacman
 # uppercase country code to set locale
 COUNTRY=FR
-#DISTRIB=console
+#DISTRIB=noobs
+#SKIP_AUR=yes
