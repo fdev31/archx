8fe12dbb1d09b369234c398e0debb53919abc627
fab <fdev31@gmail.com>
2018-06-11 00:59:38 +0200

    explicit type

--- distrib/default.sh
+++ distrib/default.sh
@@ -2,7 +2,7 @@ ENV=mate # mate or gnome
 NETMGR=networkmanager
 PROFILES="$PKG_ALL env-$ENV env-$ENV-apps env-awesome env-gnome electronic-base"
 PREFERRED_TOOLKIT=gtk
-DISTRO_PACKAGE_LIST=
+DISTRO_PACKAGE_LIST=""
 DISK_TOTAL_SIZE=4
 DISK_SQ_PART=3500 # squashfs part size in MB
 
