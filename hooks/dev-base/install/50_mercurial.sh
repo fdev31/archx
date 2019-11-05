ee18ec1134b36179ffe2b161e81cd1f50a7034eb
fdev31 <fdev31@gmail.com>
2019-11-05 21:55:35 +0100

    do not install tortoiseHg - too costly

--- hooks/dev-base/install/50_mercurial.sh
+++ hooks/dev-base/install/50_mercurial.sh
@@ -1,9 +1,10 @@
 install_pkg mercurial
 install_pkg python2-pygments
 
-if have_xorg ; then
-    for d in python2 mercurial python2-pyqt4 python2-qscintilla python2-iniparse; do
-        install_pkg --asdeps "$d"
-    done
-    have_xorg && install_pkg tortoisehg
-fi
+# Too costly!
+#if have_xorg ; then
+#    for d in python2 mercurial python2-pyqt4 python2-qscintilla python2-iniparse; do
+#        install_pkg --asdeps "$d"
+#    done
+#    have_xorg && install_pkg tortoisehg
+#fi
