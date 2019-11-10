1c969815fd2426933188ba7895b67ee890c8ab2e
fdev31 <fdev31@gmail.com>
2019-11-10 21:24:01 +0100

    initcpio conf: add modconf

--- hooks/flashdisk/pre-mkinitcpio/50_mkinitcpio_conf.sh
+++ hooks/flashdisk/pre-mkinitcpio/50_mkinitcpio_conf.sh
@@ -1,6 +1,6 @@
 append_text "/etc/mkinitcpio.conf" <<EOF
 MODULES=(squashfs vfat loop overlay btrfs ext4 ntfs)
-HOOKS=(base udev keyboard block rolinux shutdown)
+HOOKS=(base udev modconf block rolinux keyboard shutdown)
 COMPRESSION='$COMPRESSION_TYPE'
 EOF
 
