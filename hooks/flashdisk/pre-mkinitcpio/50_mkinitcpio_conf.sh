ed7fc4bca07cf191da91f44df05c07162ff6d624
fdev31 <fdev31@gmail.com>
2019-11-19 22:23:41 +0100

    force xz for initcpio (zstd not supported)

--- hooks/flashdisk/pre-mkinitcpio/50_mkinitcpio_conf.sh
+++ hooks/flashdisk/pre-mkinitcpio/50_mkinitcpio_conf.sh
@@ -1,7 +1,7 @@
 append_text "/etc/mkinitcpio.conf" <<EOF
 MODULES=(squashfs vfat loop overlay btrfs ext4 ntfs)
 HOOKS=(base udev modconf block rolinux keyboard shutdown)
-COMPRESSION='$COMPRESSION_TYPE'
+COMPRESSION='xz'
 EOF
 
 install_file resources/linux.preset "/etc/mkinitcpio.d/linux.preset"
